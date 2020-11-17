df9a19114d9d14dd150bf12d04c9d3f60440ef2f
Dominic Miglar <dominic.miglar@netunix.at>
2020-11-17 13:50:49 +0100

    added spring retry

---
+++ src/main/kotlin/com/logreposit/renogyrover/configuration/LogrepositConfiguration.kt
@@ -0,0 +1,11 @@
+package com.logreposit.renogyrover.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "logreposit")
+class LogrepositConfiguration {
+    var apiBaseUrl: String? = "https://api.logreposit.com"
+    var deviceToken: String? = "INVALID"
+}
---
+++ src/main/kotlin/com/logreposit/renogyrover/configuration/RetryConfiguration.kt
@@ -0,0 +1,8 @@
+package com.logreposit.renogyrover.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.retry.annotation.EnableRetry
+
+@Configuration
+@EnableRetry
+class RetryConfiguration
