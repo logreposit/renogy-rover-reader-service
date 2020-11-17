0f4662e45509083087a35d447e0d4eb98898e16e
Dominic Miglar <dominic.miglar@netunix.at>
2020-11-17 18:15:35 +0100

    fixed naming

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/froeling-reader-service/app.jar
+java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/renogy-rover-reader-service/app.jar
