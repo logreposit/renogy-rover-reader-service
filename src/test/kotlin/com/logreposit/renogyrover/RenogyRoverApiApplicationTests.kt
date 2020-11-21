bffd95fbe7b0cc3ee6c8f7205e8ddb2516162824
Dominic Miglar <dominic.miglar@netunix.at>
2020-11-21 17:32:51 +0100

    test configuration properties

--- src/test/kotlin/com/logreposit/renogyrover/RenogyRoverApiApplicationTests.kt
+++ src/test/kotlin/com/logreposit/renogyrover/RenogyRoverApiApplicationTests.kt
@@ -1,11 +1,33 @@
 package com.logreposit.renogyrover
 
+import com.logreposit.renogyrover.configuration.LogrepositConfiguration
+import com.logreposit.renogyrover.configuration.RenogyConfiguration
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class RenogyRoverApiApplicationTests {
+	@Autowired
+	private lateinit var logrepositConfiguration: LogrepositConfiguration
+
+	@Autowired
+	private lateinit var renogyConfiguration: RenogyConfiguration
+
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun `test logreposit configuration initialization expect values from properties file`() {
+		assertThat(logrepositConfiguration.apiBaseUrl).isEqualTo("https://api.logreposit.com")
+		assertThat(logrepositConfiguration.deviceToken).isEqualTo("INVALID")
+		assertThat(logrepositConfiguration.scrapeIntervalInMillis).isEqualTo(15000)
+	}
+
+	@Test
+	fun `test renogy configuration initialization expect values from properties file`() {
+		assertThat(renogyConfiguration.comPort).isEqualTo("/dev/ttyUSB0")
 	}
 }
